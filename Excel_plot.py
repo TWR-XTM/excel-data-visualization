047cf2bdd87ccaa60412359a52e5ae2909964b24
Utsav Maskey <35460309+TWR-XTM@users.noreply.github.com>
2018-08-08 18:20:56 +0545

    added command line arguements

--- Excel_plot.py
+++ Excel_plot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import math
+import argparse
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib import style
@@ -11,36 +12,36 @@ style.use('dark_background')
 
 # Functions
 
-def plot_annual(year, annual, sheet):
+def create_pdf():
 
-    # Plot year (x-axis) & annual (y-axis)
+     # Create multi-page PDF files to store plots
 
-    plt.plot(year,annual,"bo-",linewidth=2, markersize=3, label = "Annual")	    
-    plt.xlabel("Year")	
-    #plt.ylabel("optional")
-    plt.title('Station No. {}'.format(sheet))
-    plt.axis('auto')
-    plt.legend(loc="upper right")
-    #plt.show()    
-    plt.savefig(annual_pdf, format='pdf')
-    plt.clf()      
-        
+    annual_pdf = PdfPages('Annual.pdf')
+    c_annual_pdf = PdfPages('Cummulative annual.pdf')
+    return annual_pdf, c_annual_pdf
 
-def plot_c_annual(c_annual, sheet):
 
-    # Plot cummulative annual (y-axis)
 
-    plt.plot(c_annual,"bo-",linewidth=2, markersize=3, label = "Cumulative Annual")	
-    plt.xlabel("Year")	
-    #plt.ylabel("optional")
-    plt.title('Station No. {}'.format(sheet))
-    plt.axis('auto')
-    plt.legend(loc="upper right")
-    #plt.show()    
-    plt.savefig(c_annual_pdf, format='pdf')
-    plt.clf()
+def get_sheet_names(file_name):
 
-        
+    # Extract sheet names of excel file
+
+    xl = pd.ExcelFile(file_name)
+    xl_sheets = xl.sheet_names
+    return xl_sheets
+
+
+
+def load_data(file_name, sheet):
+
+    # Loads data of all sheets and stores into a dataframe
+
+    df = pd.read_excel(io = file_name, sheet_name = sheet, squeeze = True, header = None)
+    df = df.T    
+    return df
+
+
+
 def sheet_var(df):
 
     # Split data of a sheet into component numpy arrays
@@ -57,40 +58,100 @@ def sheet_var(df):
     year = np.array(year)
     annual = np.array(annual)
     c_annual = np.array(c_annual)
-
     return year, annual, c_annual
 
 
 
-# Create multi-page PDF files to store plots
+def plot_annual(year, annual, sheet, annual_pdf, x_label1, y_label1, line_label1, chart_title1):
 
-annual_pdf = PdfPages('Annual.pdf')
-c_annual_pdf = PdfPages('Cummulative annual.pdf')
+    # Plot year (x-axis) & annual (y-axis)
+
+    plt.plot(year,annual,"bo-",linewidth=2, markersize=3, label = line_label1)	    
+    plt.xlabel(x_label1)	
+    plt.ylabel(y_label1)
+    plt.title('{} {}'.format(chart_title1, sheet))
+    plt.axis('auto')
+    plt.legend(loc="upper right")
+    #plt.show()    
+    plt.savefig(annual_pdf, format='pdf')
+    plt.clf()      
+
+        
+
+def plot_c_annual(c_annual, sheet, c_annual_pdf, x_label2, y_label2, line_label2, chart_title2):
+
+    # Plot cummulative annual (y-axis only in default)
+
+    plt.plot(c_annual,"bo-",linewidth=2, markersize=3, label = line_label2)	
+    plt.xlabel(x_label2)	
+    plt.ylabel(y_label2)
+    plt.title('{} {}'.format(chart_title2, sheet))
+    plt.axis('auto')
+    plt.legend(loc="upper right")
+    #plt.show()    
+    plt.savefig(c_annual_pdf, format='pdf')
+    plt.clf()
 
 
-# Extract sheet names of excel file
 
-xl = pd.ExcelFile('data.xlsx')
-xl_sheets = xl.sheet_names
+def close_pdf(annual_pdf, c_annual_pdf):
+
+    # Close the files
+
+    annual_pdf.close()
+    c_annual_pdf.close()        
 
 
 
-for sheet in xl_sheets:
 
-    # Loads data per sheet and plots the data
 
-    df = pd.read_excel(io = 'data.xlsx', sheet_name = sheet, squeeze = True, header = None)
-    df = df.T    
-    year, annual, c_annual = sheet_var(df)   
-    plot_annual(year, annual, sheet)        
-    plot_c_annual(c_annual, sheet)
-    print("\nStatus:  Plotted Sheet No. {}".format(sheet))    # Status output
-    
-  
-# Close the files
+def main():
 
-annual_pdf.close()
-c_annual_pdf.close()
+    parser = argparse.ArgumentParser(description='Visualize data')
+    parser.add_argument('--file_name', type=str, default = 'data.xlsx',
+                    help='Excel file name whose data is to be visualized.')
+    parser.add_argument('--x_label1', type=str, default = 'Year',
+                    help='x-axis label for 1st line chart')
+    parser.add_argument('--y_label1', type=str, default = '',
+                    help='y-axis label for 1st line chart')
+    parser.add_argument('--x_label2', type=str, default = 'Year',
+                    help='x-axis label for 2nd line chart')
+    parser.add_argument('--y_label2', type=str, default = '',
+                    help='y-axis label for 2nd line chart')
+    parser.add_argument('--line_label1', type=str, default = 'Annual',
+                    help='Line label for 1st line chart')
+    parser.add_argument('--line_label2', type=str, default = 'Cummulative Annual',
+                    help='Line label for 2nd line chart')
+    parser.add_argument('--chart_title1', type=str, default = 'Station no.',
+                    help='Chart title for 1st line chart')
+    parser.add_argument('--chart_title2', type=str, default = 'Station no.',
+                    help='Chart title for 2nd line chart')
+    args = parser.parse_args()
+
+
+    file_name, x_label1, y_label1, x_label2, y_label2, line_label1, line_label2, chart_title1, chart_title2 = args.file_name, args.x_label1, args.y_label1, args.x_label2, args.y_label2, args.line_label1, args.line_label2, args.chart_title1, args.chart_title2
+    print('Visualizing File: {}'.format(file_name))   
+
+
+    annual_pdf, c_annual_pdf = create_pdf()
+    xl_sheets = get_sheet_names(file_name) 
+
+
+    for sheet in xl_sheets:
+        df = load_data(file_name, sheet)
+        year, annual, c_annual = sheet_var(df)   
+        plot_annual(year, annual, sheet, annual_pdf, x_label1, y_label1, line_label1, chart_title1)        
+        plot_c_annual(c_annual, sheet, c_annual_pdf, x_label2, y_label2, line_label2, chart_title2)
+        print("\nStatus:  Plotted Sheet No. {}".format(sheet))    # Status output
+        
+
+    close_pdf(annual_pdf, c_annual_pdf)
+
+
+
+if __name__ == "__main__":
+    main()
+
 
 
 
--- README.md
+++ README.md
@@ -3,11 +3,9 @@
 This python code visualizes line chart of a multi-sheet excel file having format like 'data.xlsx' file
 
 
-
 ## Requirements:
 
 * [Python 3]
-
 
 
 ## Dependencies:         
@@ -21,12 +19,28 @@ This python code visualizes line chart of a multi-sheet excel file having format
 * [xlrd]
 
 
+## Usage:
 
-## Instructions:
+1. Excel file should be in same directory as the python file.
 
-1. 'data.xlsx' file in same directory as the python file.
 2. Run the python code.
       * python Excel_plot.py
-      
+
+    Command-line arguements:
+
+      See Excel_plot.py for default arguements
+
+      * '--file_name'   : Excel file name whose data is to be visualized.         
+      * '--x_label1'    : x-axis label for 1st line chart
+      * '--y_label1'    : y-axis label for 1st line chart
+      * '--x_label2'    : x-axis label for 2nd line chart
+      * '--y_label2'    : y-axis label for 2nd line chart
+      * '--line_label1' : Line label for 1st line chart
+      * '--line_label2' : Line label for 2nd line chart
+      * '--chart_title1': Chart title for 1st line chart
+      * '--chart_title2': Chart title for 2nd line chart
+
+      Example: python Excel_plot.py --file_name 'data.xlsx' --x_label1 'Year in AD' --chart_title1 'Data over Years of Station '
+
 
 
